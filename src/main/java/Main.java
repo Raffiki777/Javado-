2a941b5c8490549c536e87c4400435ba0f0cf86f
Raffiki777 <prmalatjhb024@student.wethinkcode.co.za>
2025-02-09 13:31:28 +0200

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         TaskManager manager = new TaskManager();
         Scanner scanner = new Scanner(System.in);
